c678731239a69a770bca20a604c05aefc52e3e45
Nick Nobile <nickcnobile@gmail.com>
2020-10-04 20:58:59 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -2,11 +2,11 @@ class Post
   attr_accessor :title, :author
 
   def initialize
-    @title
-    @author
+    @title = title
+    @author = author
   end
 
 end
 
-post = Post.new("Hello World")
+title = Post.new("Hello World")
 author_new = Author.new("Uncle Bob")
