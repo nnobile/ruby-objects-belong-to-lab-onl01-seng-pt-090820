72fd6b200c690fa45c9c431e5440ba34fbd28a3a
Nick Nobile <nickcnobile@gmail.com>
2020-10-04 20:23:25 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,11 @@
 class Artist
   attr_accessor :name
 
-  def initialize
+  def initialize(name)
     @name = name
   end
 
 end
 
-artist = Artist.new
+artist = Artist.new("Beyonce")
 artist.name = "Beyonce"
